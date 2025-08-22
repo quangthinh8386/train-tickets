6067cfaa0b171ac1cfb8dc53ea58a55079c05c77
pham.quang.thinh <pham.quang.thinh@mqsolutions.com.vn>
2025-08-22 14:03:12 +0700

    upload avatar

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,34 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors, BadRequestException } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { RegisterUserDto } from 'src/user/dto/register-user.dto';
 import { LoginUserDto } from 'src/user/dto/login-user.dto';
-
+import { storage } from './oss';
+import * as path from 'path';
+import { FileInterceptor } from '@nestjs/platform-express';
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) { }
+  constructor(private readonly userService: UserService) {}
+
+  @Post('upload/avt')
+  @UseInterceptors(FileInterceptor('file', {
+    dest: 'uploads/avatar',
+    storage: storage,
+    limits: { fileSize: 1024 * 1024 * 3 },
+    fileFilter(req, file, cb) {
+      const extName = path.extname(file.originalname)
+      if (['.jpg', '.png', '.gif'].includes(extName)) {
+        cb(null, true);
+      } else {
+        cb(new BadRequestException("Upload file Error"), false);
+      }
+    }
+  }))
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    console.log(file.path);
+    return file.path
+  }
 
   @Post('login')
   login(@Body() loginUserDto: LoginUserDto) {
